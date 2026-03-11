7356e0b6ff6ad3048b5a00a0507c7d4a0174a217
02230101.cst@rub.edu.bt <02230101.cst@rub.edu.bt>
2026-03-11 14:32:51 +0600

    lab1

---
+++ 02230101_LAB1.py
@@ -0,0 +1,53 @@
+class CustomList:
+    
+    # Constructor (Task 1)
+    def __init__(self):
+        self.capacity = 10                # default capacity
+        self.arr = [None] * self.capacity # underlying array
+        self.size = 0                     # current size
+        
+        print("Created new CustomList with capacity:", self.capacity)
+        print("Current size:", self.size)
+
+    # 1. append(element)
+    def append(self, element):
+        if self.size < self.capacity:
+            self.arr[self.size] = element
+            self.size += 1
+            print("Appended", element, "to the list")
+        else:
+            print("List is full")
+
+    # 2. get(index)
+    def get(self, index):
+        if 0 <= index < self.size:
+            return self.arr[index]
+        else:
+            print("Invalid index")
+            return None
+
+    # 3. set(index, element)
+    def set(self, index, element):
+        if 0 <= index < self.size:
+            self.arr[index] = element
+            print(f"Set element at index {index} to {element}")
+        else:
+            print("Invalid index")
+
+    # 4. size()
+    def get_size(self):
+        return self.size
+
+
+# Example run
+my_list = CustomList()
+
+my_list.append(5)
+
+print("Element at index 0:", my_list.get(0))
+
+my_list.set(0, 10)
+
+print("Element at index 0:", my_list.get(0))
+
+print("Current size:", my_list.get_size())
